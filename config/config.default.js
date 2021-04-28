c4f4339e04ea0e9cc3d3f000240d48ded7623120
DamonMok <damonmok1216@gmail.com>
2021-04-28 15:08:01 +0800

    统一错误处理中间件

--- config/config.default.js
+++ config/config.default.js
@@ -16,7 +16,7 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1619507994505_3900';
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = ['errorHandler'];
 
   config.mysql = {
     client: {
