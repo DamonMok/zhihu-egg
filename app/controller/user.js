509ced1c8c80d6c253ed31b1ec124098a67ebcf4
DamonMok <damonmok1216@gmail.com>
2021-04-28 16:11:47 +0800

    添加数据验证插件

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,13 +1,27 @@
 'use strict';
-
 const Controller = require('egg').Controller;
+
+
+const registerRule = {
+  userName: {
+    type: 'string',
+    required: true
+  },
+  password: {
+    type: 'string',
+    required: true
+  }
+}
 
 class UserController extends Controller {
   async register() {
     const { ctx } = this
+    ctx.validate(registerRule, ctx.request.body)
+
     let { userName, password } = ctx.request.body
     password = ctx.helper.handlePassword(password)
 
+    // ctx.throw(422, '奇怪的错误')
 
     ctx.body = `${userName}: ${password}`
   }
